f6525fffa7457d78ef651e4644189a5b073c45e6
toshimaru <me@toshimaru.net>
2023-04-17 08:39:24 +0900

    refactor: call schema directly

--- src/ActionValidator.js
+++ src/ActionValidator.js
@@ -1,12 +1,8 @@
 const actionSchema = require('./schemas/actionSchema');
 
 class ActionValidator {
-  static get #schema() {
-    return actionSchema;
-  }
-
   static async validate(input) {
-    const validatedConfig = await this.#schema.validateAsync(input, {
+    const validatedConfig = await actionSchema.validateAsync(input, {
       abortEarly: false
     });
     return validatedConfig;
