443828a75a0637af7d9c38a33cefa7dc009875fb
toshimaru <me@toshimaru.net>
2023-04-23 04:30:06 +0900

    test: Add `performActions` test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,38 @@
 const App = require("../src/App");
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const coreSpy = jest.spyOn(core, 'debug');
+
+function mockContent(content) {
+    const getOctokit = jest.fn(() => {
+        const getContent = jest.fn(() => {
+            return {
+                data: {
+                    content:  Buffer.from(content).toString('base64')
+                },
+            };
+        });
+        return {
+            rest: {
+                repos: { getContent: getContent },
+            },
+        };
+    });
+    jest.spyOn(github, 'getOctokit').mockImplementation(getOctokit);
+}
 
 describe("App", () => {
-    it("should be defined", () => {
-        expect(App).toBeDefined();
+    describe("performActions", () => {
+        it('finds no actions', async () => {
+            const config = { 'github-token': 'dummy-token' };
+            jest.replaceProperty(github, 'context', { payload: { label: { name: '' } } });
+            mockContent('test:\n  comment: hello');
+    
+            const app = new App(config);
+            await app.performActions();
+            expect(coreSpy).toHaveBeenCalledWith('No actions found');
+            expect(github.getOctokit).toHaveBeenCalledWith(config['github-token']);
+        });
     });
 });
