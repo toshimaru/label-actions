95a10b2b8d8a009e2f0a0fa8f48084c8d3d784b6
Toshimaru <me@toshimaru.net>
2023-04-27 04:37:51 +0900

    fix test: "triggers `comment` action" (#86)
    * fix: Add `jest.restoreAllMocks()` and fix `jest.spyOn`
    
    * test: better tests for commenting
    
    * test: Add issue-author replace test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,8 +2,6 @@ const App = require("../src/App");
 const github = require('@actions/github');
 const core = require('@actions/core');
 
-const coreSpy = jest.spyOn(core, 'debug');
-
 function mockContent(content, additionalRestMethods = {}) {
     const getOctokit = jest.fn(() => {
         const getContent = jest.fn(() => {
@@ -25,26 +23,41 @@ function mockContent(content, additionalRestMethods = {}) {
 
 describe("App", () => {
     describe("performActions", () => {
+        afterEach(() => jest.restoreAllMocks());
+
         it('finds no actions', async () => {
             const config = { 'github-token': 'dummy-token' };
+            const debugLogSpy = jest.spyOn(core, 'debug');
             jest.replaceProperty(github, 'context', { payload: { label: { name: '' } } });
             mockContent('test:\n  comment: hello');
     
             const app = new App(config);
             await app.performActions();
-            expect(coreSpy).toHaveBeenCalledWith('No actions found');
             expect(github.getOctokit).toHaveBeenCalledWith(config['github-token']);
+            expect(debugLogSpy).toHaveBeenCalledWith('No actions found');
         });
 
         it('triggers `comment` action', async () => {
             const config = { 'github-token': 'dummy-token' };
-            jest.replaceProperty(github, 'context', { payload: { label: { name: 'test' }, issue: { user: {} } }, repo: { owner: 'toshimaru' } });
-            mockContent('test:\n  comment: hello', { issues: { createComment: jest.fn() } });
+            const debugLogSpy = jest.spyOn(core, 'debug');
+            const context = { 
+                payload: {
+                    label: { name: 'test' }, 
+                    issue: { user: { login: 'test-user' }, number: 1 } 
+                },
+                repo: { owner: 'toshimaru', repo: 'my-repo' }
+            };
+            jest.replaceProperty(github, 'context', context);
+            const createComment = jest.fn();
+            mockContent('test:\n  comment: "@{issue-author} hello"', { issues: { createComment: createComment } });
     
             const app = new App(config);
             await app.performActions();
-            expect(coreSpy).toHaveBeenCalledWith('No actions found');
             expect(github.getOctokit).toHaveBeenCalledWith(config['github-token']);
+            expect(debugLogSpy).toHaveBeenCalledWith('Commenting');
+            expect(createComment).toHaveBeenCalledWith({
+                owner: context.repo.owner, repo: context.repo.repo, issue_number: context.payload.issue.number, body: '@test-user hello'
+            });
         });
     });
 });
